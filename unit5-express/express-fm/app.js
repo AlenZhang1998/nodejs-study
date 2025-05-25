45892b7c7cccf953d9d03f9abf20867e1ad7be84
ZhangMin1998 <1464253423@qq.com>
2025-05-25 23:08:24 +0800

    upd: 5-4Express管理用户数据信息

--- unit5-express/express-fm/app.js
+++ unit5-express/express-fm/app.js
@@ -1,12 +1,30 @@
 // 引入express模块
 const express = require('express');
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
 
 // 创建服务器
 const app = express();
 
 // 监听请求
-app.get('/', (req, res) => {
-  res.send('hello express')
+// app.get('/', (req, res) => {
+//   fs.readFile('./db.json', (err, data) => {
+//     if (err) {
+//       res.status(500).json({err})
+//       // res.send('404 not found');
+//     } else {
+//       res.send(JSON.parse(data).users);
+//     }
+//   })
+// })
+app.get('/', async (req, res) => {
+  try {
+    const back = await readFile('./db.json', 'utf-8');
+    res.send(JSON.parse(back).users);
+  } catch (error) {
+    res.status(500).json({err})
+  }
 })
 
 app.listen(4444, () => {
