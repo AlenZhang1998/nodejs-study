1896e38e1cedd67e156fc3577e780ff17a04ab01
ZhangMin1998 <1464253423@qq.com>
2025-05-25 15:23:19 +0800

    upd: 4-3服务器响应不同数据类型

--- unit4/server.js
+++ unit4/server.js
@@ -1,5 +1,6 @@
 // 导入http模块
 var http = require('http')
+var fs = require('fs')
 
 // 创建http服务
 // 获取到服务器实例对象
@@ -9,8 +10,26 @@ server.listen(8080, function () {
 })
 
 // 监听request请求事件
+// server.on('request', function (req, res) {
+//     console.log('request')
+//     // res.setHeader('Content-type', 'text/plain;charset=utf-8') // 响应头
+//     res.setHeader('Content-Type', 'text/html;charset=utf-8') // 响应头
+//     res.write(`hello, <h1>你好</h1>`) // 响应数据
+//     res.end() // 断开服务器
+// })
+
 server.on('request', function (req, res) {
-    console.log('request')
-    res.write('hello, 你好') // 响应数据
-    res.end() // 断开服务器
+    if (req.url === '/') {
+        fs.readFile('./index.html','utf-8', function (err, data) {
+            if (err) {
+                res.end('404 Not Found')
+            } else {
+                // res.write(data)
+                res.end(data)
+            }
+        })
+    } else {
+        res.write('hello')
+        res.end() // 断开服务器
+    }
 })
