9cda24fe4c763705713c07e07e2a3a1f9ad5b73d
ZhangMin1998 <1464253423@qq.com>
2025-05-25 17:07:42 +0800

    upd: 4-4HTTP的不同请求方法处理

---
+++ unit4/request-method.js
@@ -0,0 +1,34 @@
+// 导入http模块
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+// 创建http服务
+// 获取到服务器实例对象
+var server = http.createServer()
+server.listen(8080, function () {
+    console.log('http://127.0.0.1:8080, 服务器启动成功')
+})
+
+server.on('request', function (req, res) {
+  console.log(req.method)
+  if (req.method === 'GET') {
+    console.log(url.parse(req.url, true))
+    if (req.url === '/') {
+      fs.readFile('./index.html','utf-8', function (err, data) {
+          if (err) {
+              res.end('404 Not Found')
+          } else {
+              // res.write(data)
+              res.end(data)
+          }
+      })
+  } else {
+      res.write('hello')
+      res.end() // 断开服务器
+  }
+  } else if (req.method === 'POST') {
+      res.write('post')
+      res.end() // 断开服务器
+  }
+})
