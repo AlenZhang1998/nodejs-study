e4c209d346e98ac6a87a452f84bedde39940b3ca
ZhangMin1998 <1464253423@qq.com>
2025-05-26 00:02:09 +0800

    upd: 5-6添加用户信息到db文件

--- unit5-express/express-fm/app.js
+++ unit5-express/express-fm/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 // 创建服务器
 const app = express();
@@ -25,15 +26,36 @@ app.get('/', async (req, res) => {
     const back = await readFile('./db.json', 'utf-8');
     res.send(JSON.parse(back).users);
   } catch (error) {
-    res.status(500).json({err})
+    res.status(500).json({error})
   }
 })
 
 app.post('/', async (req, res) => {
-  console.log(req.headers);
-  console.log(req.query);
-  console.log(req.body);
-  res.send('post请求');
+  let body = req.body
+  if (!body) {
+    res.status(403).json({
+      code: 403,
+      msg: '请求参数错误'
+    })
+  }
+  let back = await readFile('./db.json', 'utf-8');
+  let jsonObj = JSON.parse(back)
+  // 给输入的用户信息添加个id
+  body.id = jsonObj.users[jsonObj.users.length - 1].id + 1
+  jsonObj.users.push(body)
+  // console.log(jsonObj);
+
+  // 写入文件
+  try {
+    const w = await writeFile('./db.json', JSON.stringify(jsonObj));
+    if(!w) {
+      res.status(200).send({
+        msg: '添加成功'
+      })
+    }
+  } catch (error) {
+    res.status(500).json({error})
+  }
 })
 
 app.listen(4444, () => {
