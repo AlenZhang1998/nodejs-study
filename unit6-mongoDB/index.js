5018b1fbcf3444fa1fd18f1c18eb58eac6eea54f
ZhangMin1998 <1464253423@qq.com>
2025-05-28 00:06:46 +0800

    upd: 6-7使用Node进行增删改查

--- unit6-mongoDB/index.js
+++ unit6-mongoDB/index.js
@@ -2,11 +2,31 @@ const { MongoClient} = require('mongodb')
 
 const client = new MongoClient('mongodb://localhost:27017')
 
-const main = async () => {
+const clientFunc = async (c) => {
   await client.connect()
+  console.log('连接成功')
   const db = client.db('mytest')
-  const cc = db.collection('cc')
+  return db.collection(c)
+}
+
+const main = async () => {
+  const cc = await clientFunc('cc')
+  // 查询d
   const d = await cc.find()
+
+  // 插入
+  // const d = await cc.insertOne({name: 'cc', age: 18})
+  // const d = await cc.insertMany([
+  //   {name: 'dd', age: 22},
+  //   {name: 'ee', age: 33},
+  // ])
+
+  // 更新
+  // const d = await cc.updateOne({name: 'cc'}, {$set: {age: 22}})
+
+  // 删除
+  // const d = await cc.deleteOne({k: 4})
+
   console.log(await d.toArray())
 }
 
