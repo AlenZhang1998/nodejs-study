3896ea30c7ac79ff9937941729e05346519c3548
ZhangMin1998 <1464253423@qq.com>
2025-06-04 23:15:15 +0800

    upd: 7-1Express中间件的概念与基本应用

--- unit7-express-project/app.js
+++ unit7-express-project/app.js
@@ -6,10 +6,48 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // 挂载路由
-app.use('/api', router)
+// app.use('/api', router)
 
 // 挂载统一处理服务端错误中间件
-app.use(errorHandler())
+// app.use(errorHandler())
+
+// function logs(req) {
+//   console.log(`${req.method}, ${req.url}, ${Date.now()}`);
+//   // console.log(`${req.method}, ${req.url}, ${Date.now()}`);
+//   // console.log(`${req.method}, ${req.url}, ${Date.now()}`);
+// }
+
+// app.get('/', (req, res) => {
+//   logs(req)
+//   res.send('/index')
+// })
+
+// app.get('/register', (req, res) => {
+//   logs(req)
+//   res.send('/iregisterdex')
+// })
+
+// app.get('/login', (req, res) => {
+//   logs(req)
+//   res.send('/login')
+// })
+
+app.use((req, res, next) => {
+  console.log(`${req.method}, ${req.url}, ${Date.now()}`);
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('/index')
+})
+
+app.get('/register', (req, res) => {
+  res.send('/iregisterdex')
+})
+
+app.get('/login', (req, res) => {
+  res.send('/login')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
