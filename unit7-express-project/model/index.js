01ff8eb72773e41bfe91d25e9328d0be9e716fef
ZhangMin1998 <1464253423@qq.com>
2025-06-10 23:29:28 +0800

    upd: 7-6数据处理模块-mongoose

---
+++ unit7-express-project/model/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/mytest')
+}
+
+// 创建一个用户集合
+// 定义模型
+const user = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+})
+
+const userModel = mongoose.model('User', user) // 第一个参数是集合的名字+s
+const u = new userModel({
+  name: '张三',
+  age: 18
+})
+
+u.save().then(res => {
+  console.log(666, res)
+})
+
+main().then(res => {
+  console.log(res, 'mongo连接成功')
+}).catch(err => {
+  console.log(err,'mongo连接失败')
+})
