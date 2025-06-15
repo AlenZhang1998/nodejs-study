f20806130b808e5c69067efa964161e25204fb7e
ZhangMin1998 <1464253423@qq.com>
2025-06-15 23:15:05 +0800

    upd: 7-9客户端提交数据安全校验-4-Running validations imperatively

--- unit7-express-project/middleware/validator/userValidator.js
+++ unit7-express-project/middleware/validator/userValidator.js
@@ -1,7 +1,8 @@
 const { body } = require('express-validator')
+const validate = require('./errorBack')
 
-module.exports.register = [
+module.exports.register = validate([
   body('username').notEmpty().withMessage('用户名不能为空'),
   body('password').notEmpty().withMessage('密码不能为空'),
   body('age').notEmpty().withMessage('请输入年龄').bail().isLength({min: 1, max: 3}).withMessage('长度不能大于3')
-]
+])
--- unit7-express-project/router/user.js
+++ unit7-express-project/router/user.js
@@ -8,14 +8,8 @@ const validator = require('../middleware/validator/userValidator')
 router
 .post('/register',
   // body('age').notEmpty().withMessage('请输入年龄').isLength({min: 1, max: 3}).withMessage('长度不能大于3'),
-  ...validator.register,
-  (req, res, next) => {
-    const err = validationResult(req);
-    if (!err.isEmpty()) {
-      return res.status(401).json({error: err.array()})
-    }
-    next()
-  },userController.register)
+  validator.register,
+  userController.register)
 .get('/list', userController.list)
 .delete('/', userController.delete)
 
