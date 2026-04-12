bec8a5c411cd91893339bf3032cc385f4ad6875c
ZhangMin1998 <1464253423@qq.com>
2026-04-12 16:38:46 +0800

    upd: 2-3文件同步异步读取

--- unit2/read.js
+++ unit2/read.js
@@ -1,8 +1,16 @@
 var fs = require("fs")
 // console.log(fs)
+
+// 异步读取
 fs.readFile('./a.txt', 'utf8', function(err, data) {
   if (err) {
     return console.error(err);
   }
   console.log("异步读取: " + data);
 })
+
+// 同步读取
+var data = fs.readFileSync('./a.txt');
+console.log("同步读取: " + data.toString());
+
+console.log("程序执行完毕。");
