01bc0422b4c05af298b6544705faaf501d05f55c
ZhangMin1998 <1464253423@qq.com>
2026-04-12 16:46:53 +0800

    upd: 2-3文件同步异步读取和写入

--- unit2/read.js
+++ unit2/read.js
@@ -14,3 +14,7 @@ var data = fs.readFileSync('./a.txt');
 console.log("同步读取: " + data.toString());
 
 console.log("程序执行完毕。");
+
+// 同步读取: 1111122222
+// 程序执行完毕。
+// 异步读取: 1111122222
--- unit2/write.js
+++ unit2/write.js
@@ -1,7 +1,21 @@
 var fs = require("fs")
-fs.writeFile('./a.txt', '11111', function(err){
+
+// 异步写入
+fs.writeFile('./a.txt', '123', function(err){
   if (err) {
     return console.error(err)
   }
   console.log("数据写入成功！") 
 })
+
+// 同步写入
+try {
+  fs.writeFileSync('./a.txt', 'Hello, World!');
+  console.log('File written successfully');
+} catch (err) {
+  console.error('Error writing file:', err);
+}
+
+// File written successfully
+// 数据写入成功！
+// 123lo, World!
