b5f284406afde3c0225a182e5c0bd7f8f5fd6ab1
Alen Zhang <alenzhang@yflife.com>
2026-04-23 12:47:45 +0800

    upd: add comment

--- unit7-express-project/model/subscribeModel.js
+++ unit7-express-project/model/subscribeModel.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose')
 const baseModel = require('./baseModel')
 const subscribeScheme = new mongoose.Schema({
-  user: { // 
+  user: { // 发起关注的人 / 粉丝
     type: mongoose.ObjectId,
     required: true,
-    ref: 'User' // 和userModel关联 表示“视频属于哪个用户”
+    ref: 'User'
   },
-  channel: {
+  channel: { // 被关注的人 / 频道
     type: mongoose.ObjectId,
     required: true,
     ref: 'User'
   },
   ...baseModel
-  
 })
 
 module.exports = subscribeScheme
+
+
+
+// 张三关注了李四
+// {
+//   user: 张三的id,
+//   channel: 李四的id
+// }
