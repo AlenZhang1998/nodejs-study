7899fb9ec30bb176fca3d77dcf4a4601f23982d7
Alen Zhang <alenzhang@yflife.com>
2026-04-27 18:25:51 +0800

    upd: 9-8使用Node操作Redis-2

--- unit9-ioredis/index.js
+++ unit9-ioredis/index.js
@@ -5,8 +5,11 @@ const redis = new Redis();
 async function run() {
   try {
     await redis.set("mykey", "value");
-    const value = await redis.get("mykey");
-    console.log(value);
+    // const value = await redis.get("mykey");
+    // console.log(value);
+    // const res = await redis.keys("*");
+    const res = await redis.smembers("list1");
+    console.log(res);
   } finally {
     redis.disconnect();
   }
